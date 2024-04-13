a95baf0c331960beeccc2e45ae3d42808d44a920
nguyenly652002 <nguyenly652002@gmail.com>
2024-04-13 14:51:47 +0700

    13/04

--- MVC/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC.Models;
-
-public class Person {
-    public string? PersonId {get; set;}
-    public string? FullName {get; set;}
-    
-    public string? Address {get; set;}
-   
-}
