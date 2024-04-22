11688f9013c9440ccf4327fba0fb749232cef58f
nguyenly652002 <nguyenly652002@gmail.com>
2024-04-22 20:20:59 +0700

    kế thừa

--- DemoMVC/Data/ApplicationDbContext.cs
+++ DemoMVC/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace DemoMVC.Data
         {}
         public DbSet<Person> Person { get; set;}
         public DbSet<Employee> Employee { get; set;}
+        public DbSet<Student> Student { get; set;}
 
        
     }
--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0feff8471ec6baefbb8ccbf33ece488264541fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f4768bf2803b961566bb60a72741faf7887278")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
