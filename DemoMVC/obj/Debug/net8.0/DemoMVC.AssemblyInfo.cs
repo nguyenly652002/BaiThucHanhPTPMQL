b10e2ec17006a09b0c9d65e839dbb7af5108da83
nguyenly652002 <nguyenly652002@gmail.com>
2024-05-31 09:09:33 +0700

    31/5

--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea897417120f48747191e946f81873e0ed9313c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7ede151f04853013499f9ecdd24ca89fa9a176")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
